d753dbe4a6120477b941efdbb4d93115817e402f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-02-25 16:28:06 -0500

    Refactor App to class component. Invoke callbacks in children.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import SearchBar from './SearchBar'
-const App = () => {
-    return (
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+    render() {
+        return(
         <div className="ui container" style={{marginTop :'10px'}}>
-            <SearchBar />
+            <SearchBar onSubmit={this.onSearchSubmit} />
         </div>
-    );
-};
+        );
+    };
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,6 +7,8 @@ class SearchBar extends React.Component {
 
     onFormSubmit = (event) => {
         event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
     
     };
     render() {
