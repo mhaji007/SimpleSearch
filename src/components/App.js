95da686312a166584b2e62b950bea605fa0e6fd9
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-02-26 11:40:06 -0500

    Handle request with async await. Set state after async request.

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,30 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos',{
+
+     // initialize state with an empty array of images
+    state = {images:[]};
+    
+    // Make call to API
+    async onSearchSubmit(term) {
+       const response = await axios.get('https://api.unsplash.com/search/photos',{
             params: {query: term},
             headers:{
                 Authorization: 'Client-ID 3Xmtoa5J-Amw2moRXwcUU9Kxp7qHAeJj6RxTsTuhF1E'
 
             }
         });
+
+        this.setState({images: response.data.results});
     }
+
     render() {
         return(
         <div className="ui container" style={{marginTop :'10px'}}>
             <SearchBar onSubmit={this.onSearchSubmit} />
+            Found:{this.state.images.length} images
         </div>
         );
     };
