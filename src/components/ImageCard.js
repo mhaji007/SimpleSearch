3aad3058cdddb4c34815f388ce8e37845b2c3f51
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-02-27 19:42:38 -0500

    Use Refs to access the image height

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
+// This component is going to be responsible for rendering one image at a time.
+// Once the image is rendered, this component is going to detemine how large the
+// image is and then adjust the grid-row-end rule to accomaodate for that image
+// size.
+
 class ImageCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        console.log(this.imageRef);
+    }
+
     render() {
         const {description, urls} = this.props.image;
 
         return (
             <div>
-                <img alt={description} src={urls.regular}/>
+                <img ref={this.imageRef} alt={description} src={urls.regular}/>
             </div>
         );
     }
